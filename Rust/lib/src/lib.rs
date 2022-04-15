bc7f7a8cd1bfe5402cb9ec53bcc3507dc0d61d72
Luca <b.lucab1211@gmail.com>
2022-04-15 16:54:55 +0200

    improved performance

--- Rust/lib/src/lib.rs
+++ Rust/lib/src/lib.rs
@@ -1,11 +1,14 @@
 mod tcp_client;
+mod chunk_handle;
 
 use tcp_client::TcpClient;
+use chunk_handle::ChunkHandle;
 
 use gdnative::prelude::*;
 
 fn init(handle: InitHandle) {
     handle.add_class::<TcpClient>();
+    handle.add_class::<ChunkHandle>();
 }
 
 godot_init!(init);
--- Rust/lib/src/tcp_client/mod.rs
+++ Rust/lib/src/tcp_client/mod.rs
@@ -8,16 +8,14 @@ use protocol::chunk::CHUNK_SIZE;
 use tokio::runtime::Runtime;
 use gdnative::prelude::*;
 
-/// The TcpClient "class"
 #[derive(NativeClass)]
 #[inherit(Node)]
 pub struct TcpClient {
     bridge: Option<Bridge>,
     runtime: Runtime,
 
-    // they need special optimisation
-    pub chunk_events: Vec<(Vec<i64>, Vec<u8>)>,
-    pub events: Vec<(String, Vec<Variant>)>,
+    events: Vec<(String, Vec<Variant>)>,
+    chunk_updates: Vec<ByteArray>,
 }
 
 #[methods]
@@ -32,8 +30,8 @@ impl TcpClient {
             bridge: None,
             runtime,
 
-            chunk_events: Vec::new(),
             events: Vec::new(),
+            chunk_updates: Vec::new(),
         }
     }
 
@@ -53,16 +51,26 @@ impl TcpClient {
     }
 
     #[export]
+    fn fetch_event(&mut self, _owner: &Node) -> Option<(String, Vec<Variant>)> {
+        self.events.pop()
+    }
+
+    #[export]
+    fn fetch_chunk_update(&mut self, _owner: &Node) -> Option<ByteArray> {
+        self.chunk_updates.pop()
+    }
+
+    #[export]
     fn send(&self, _owner: &Node, event: String) -> bool {
         if let Some(bridge) = &self.bridge {
-            let event: Result<ClientToServer, serde_json::Error> = serde_json::from_str(&event);
+            let ev: Result<ClientToServer, serde_json::Error> = serde_json::from_str(&event);
 
-            match event {
+            match ev {
                 Ok(event) => {
                     bridge.send(event.clone()); 
                 }
                 Err(e) => {
-                    godot_print!("attempted to send invalid tcp event:\n{}", e);
+                    godot_print!("attempted to send invalid tcp event:\n{}\n{:?}", e, event);
                 }
             }
             return true;
@@ -72,50 +80,28 @@ impl TcpClient {
     }
 
     #[export]
-    fn fetch_event(&mut self, _owner: &Node) -> Option<(String, Vec<Variant>)> {
-        self.events.pop()
-    }
-
-    #[export]
-    fn fetch_chunk_event(&mut self, _owner: &Node) -> Option< (Vec<i64>, Vec<u8>) > {
-        self.chunk_events.pop()
-    }
-
-    #[export]
     fn _process(&mut self, _owner: &Node, _dt: f64) {
         if let Some(bridge) = &self.bridge {
             if let Some(event) = bridge.receive() {
-
                 match event {
                     ServerToClient::ChunkUpdate(chunk) => {
-                        let pos: Vec<i64> = vec![
-                            chunk.coord[0], 
-                            chunk.coord[1], 
-                            chunk.coord[2]
-                        ];
-                        let mut data: Vec<u8> = vec![0; CHUNK_SIZE.pow(3)];
+                        let mut data: ByteArray = PoolArray::from_vec(vec![0; CHUNK_SIZE.pow(3)]);
                         for x in 0..CHUNK_SIZE {
                             for y in 0..CHUNK_SIZE {
                                 for z in 0..CHUNK_SIZE {
                                     // https://coderwall.com/p/fzni3g/bidirectional-translation-between-1d-and-3d-arrays
                                     let i = x + y * CHUNK_SIZE + z * CHUNK_SIZE * CHUNK_SIZE;
-                                    data[i] = chunk.data[x][y][z].to_category().0;
+                                    data.set(i as i32, chunk.data[x][y][z].to_category().0);
                                 }
                             }
                         }
-                        self.chunk_events.push( (pos, data) );
+                        self.chunk_updates.push(data);
                     }
                     ServerToClient::Token(token) => {
-                        self.events.push((
-                            String::from("Token"),
-                            vec![Variant::new(token.to_vec())]
-                        ));
+                        self.events.push( (String::from("Token"), vec![Variant::new(token.to_vec())]) );
                     }
                     ServerToClient::Error(error) => {
-                        self.events.push((
-                            String::from("Error"),
-                            vec![Variant::new(format!("{:?}", error))]
-                        ));
+                        self.events.push( (String::from("Error"), vec![Variant::new(format!("{:?}", error))]) );
                     }
                 }
             }
